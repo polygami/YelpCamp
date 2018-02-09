3dbe7de19967efebea84f61d815eaa4e7a9991f6
Liam Owen <liam@polygami.co.uk>
2018-02-09 14:04:13 +0100

    - add middleware - add author to campground created in post route

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -22,11 +22,15 @@ router.get("/", function(req, res) {
 });
 
 // CREATE - Adds a new campground to the database
-router.post("/", function (req, res) {
+router.post("/", isLoggedIn, function (req, res) {
 	var name = req.body.name;
 	var image = req.body.image;
 	var description = req.body.description;
-	var newCampground = { name: name, image: image, description: description };
+	var author = {
+		id: req.user._id,
+		username: req.user.username
+	}
+	var newCampground = { name: name, image: image, description: description, author: author };
 
 	Campground.create(newCampground, function (err, campground) {
 		if (err) {
@@ -38,7 +42,7 @@ router.post("/", function (req, res) {
 });
 
 // NEW - Shows a form to add a new campground
-router.get("/new", function (req, res) {
+router.get("/new", isLoggedIn, function (req, res) {
 	res.render("campgrounds/new");
 });
 
@@ -52,6 +56,18 @@ router.get("/:id", function(req, res) {
 		}
 	});
 });
+
+/////////////////////////////////////////////////
+//                  MIDDLEWARE                 //
+/////////////////////////////////////////////////
+
+// Checks if the user is logged in
+function isLoggedIn(req, res, next){
+	if (req.isAuthenticated()){
+		return next();
+	}
+	res.redirect("/login");
+}
 
 /////////////////////////////////////////////////
 //                   EXPORTS                   //
