50f1b02ca665f2d2aeb957ebc0b918c9baa142c1
Liam Owen <liam@polygami.co.uk>
2018-02-13 11:47:58 +0100

    add comment EDIT and UPDATE routes

--- routes/comments.js
+++ routes/comments.js
@@ -50,6 +50,28 @@ router.post("/", isLoggedIn, function(req, res) {
 	});
 });
 
+// EDIT
+router.get("/:comment_id/edit", function(req, res) {
+	Comment.findById(req.params.comment_id, function(err, comment) {
+		if(err){
+			res.redirect("back");
+		} else {
+			res.render("comments/edit", {comment: comment, campground_id: req.params.id})
+		}
+	});
+});
+
+// UPDATE
+router.put("/:comment_id", function(req, res) {
+	Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, comment) {
+		if(err) {
+			res.redirect("back");
+		} else {
+			res.redirect("/campgrounds/" + req.params.id);
+		}
+	})
+});
+
 /////////////////////////////////////////////////
 //                  MIDDLEWARE                 //
 /////////////////////////////////////////////////
